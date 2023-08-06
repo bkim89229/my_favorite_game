c1f1b18ccb54fea18ee3b85309f61fd9dd89971b
bkim <126510650+bkim89229@users.noreply.github.com>
2023-08-06 01:56:24 -0400

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,45 @@
+// Attach reviewData to the global window Object
+// so other scripts can access it at runtime.
+
+window.reviewData = [
+  {
+    id: "r01",
+    name: "Kim Eun-joo",
+    date: "2023-08-01",
+    rating: 2,
+    text: "Didn't really enjoy the game. It's not my cup of tea.",
+    flag: false
+  },
+  {
+    id: "r02",
+    name: "Lee Jeong-hoon",
+    date: "2023-08-02",
+    rating: 4,
+    text: "Great game!",
+    flag: false
+  },
+  {
+    id: "r03",
+    name: "Park Ji-hye",
+    date: "2023-08-03",
+    rating: 3,
+    text: "It's an okay game, but it should be improved more.",
+    flag: false
+  },
+  {
+    id: "r04",
+    name: "Han Ji-hoon",
+    date: "2023-08-04",
+    rating: 5,
+    text: "Absolutely loved it! Best game I've played in years!",
+    flag: false
+  },
+  {
+    id: "r05",
+    name: "Jeong Yoo-jin",
+    date: "2023-08-05",
+    rating: 1,
+    text: "Don't waste your time. I should've played other games.",
+    flag: false
+  }
+];
